2fda32b8d1b3d9d6954bff0e2ee8bded34e1a923
Oliver Günther <mail@oliverguenther.de>
2014-09-30 16:29:47 +0200

    Fix for dangling association on role update.

--- lib/open_project/revisions/git/hooks.rb
+++ lib/open_project/revisions/git/hooks.rb
@@ -4,7 +4,7 @@ module OpenProject::Revisions::Git::Hooks
   class RoleUpdater < Redmine::Hook::Listener
     def roles_changed(context)
       message = context[:message]
-      projects = Project.active.includes(:repositories).all
+      projects = Project.active.includes(:repository).all
       if projects.length > 0
         OpenProject::Revisions::Git::GitoliteWrapper.logger.info("Role has been #{message}, resync all projects...")
         OpenProject::Revisions::Git::GitoliteWrapper.update(:update_all_projects, projects.length)
