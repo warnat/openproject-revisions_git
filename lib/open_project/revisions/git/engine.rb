1a9e3e8c9c8b5833810cf594073bf20976b79f5a
José Luis González García <jose-luis.gonzalez-garcia@gwdg.de>
2016-03-03 10:08:18 +0100

    Adding some identation

--- lib/open_project/revisions/git/engine.rb
+++ lib/open_project/revisions/git/engine.rb
@@ -18,27 +18,27 @@ module OpenProject::Revisions::Git
         {
           
           # Gitolite SSH Config
-          gitolite_user:                  'git',
-          gitolite_server_host:           default_hostname,
-          gitolite_server_port:           '22',
-          gitolite_ssh_private_key:       File.join(Dir.home, '.ssh', 'id_rsa').to_s,
-          gitolite_ssh_public_key:        File.join(Dir.home, '.ssh', 'id_rsa.pub').to_s,
+          gitolite_user:                         'git',
+          gitolite_server_host:                  default_hostname,
+          gitolite_server_port:                  '22',
+          gitolite_ssh_private_key:              File.join(Dir.home, '.ssh', 'id_rsa').to_s,
+          gitolite_ssh_public_key:               File.join(Dir.home, '.ssh', 'id_rsa.pub').to_s,
           
           # Gitolite Storage Config
           # deprecated
-          gitolite_global_storage_dir:    'repositories',
+          gitolite_global_storage_dir:           'repositories',
           # Full path
-          gitolite_global_storage_path:   '/home/git/repositories',
-          gitolite_redmine_storage_dir:   '',
-          gitolite_recycle_bin_dir:       'recycle_bin/',
-          gitolite_local_code_dir:        '.gitolite/',
-          gitolite_lib_dir:               'bin/lib/',
+          gitolite_global_storage_path:          '/home/git/repositories',
+          gitolite_redmine_storage_dir:          '',
+          gitolite_recycle_bin_dir:              'recycle_bin/',
+          gitolite_local_code_dir:               '.gitolite/',
+          gitolite_lib_dir:                      'bin/lib/',
           
           # Gitolite Config File
-          gitolite_admin_dir:                File.join(Dir.home, 'gitolite-admin'),
-          gitolite_config_file:              'gitolite.conf',
-          gitolite_identifier_prefix:        'openproject_',
-          gitolite_identifier_strip_user_id: false,
+          gitolite_admin_dir:                    File.join(Dir.home, 'gitolite-admin'),
+          gitolite_config_file:                  'gitolite.conf',
+          gitolite_identifier_prefix:            'openproject_',
+          gitolite_identifier_strip_user_id:     false,
           
           # Gitolite Global Config
           gitolite_temp_dir:                     File.join(Dir.home, 'tmp', 'openproject_revisions_git').to_s,
@@ -46,47 +46,47 @@ module OpenProject::Revisions::Git
           gitolite_log_level:                    'info',
           gitolite_log_split:                    false,
           git_config_username:                   'OpenProject Revisions(Git)',
-          git_config_email:                      'openproject@localhost',
+          git_config_email:                      'openproject@example.net',
           gitolite_scripts_dir:                  File.join(Dir.home, 'bin'),
           gitolite_timeout:                      10,
           gitolite_resync_all:                   false,
           
           # Gitolite Hooks Config
-          gitolite_overwrite_existing_hooks: true,
-          gitolite_hooks_are_asynchronous:   false,
-          gitolite_hooks_debug:              false,
-          gitolite_hooks_url:                'http://localhost:3000',
+          gitolite_overwrite_existing_hooks:     true,
+          gitolite_hooks_are_asynchronous:       false,
+          gitolite_hooks_debug:                  false,
+          gitolite_hooks_url:                    'http://localhost:3000',
           
           # Gitolite Cache Config
-          gitolite_cache_max_time:          '86400',
-          gitolite_cache_max_size:          '16',
-          gitolite_cache_max_elements:      '2000',
-          gitolite_cache_adapter:           'database',
+          gitolite_cache_max_time:               '86400',
+          gitolite_cache_max_size:               '16',
+          gitolite_cache_max_elements:           '2000',
+          gitolite_cache_adapter:                'database',
           
           
           # Gitolite Access Config
-          ssh_server_domain:                default_hostname,
-          http_server_domain:               default_hostname,
-          https_server_domain:              default_hostname,
-          http_server_subdir:               '',
-          show_repositories_url:            true,
-          gitolite_daemon_by_default:       false,
-          gitolite_http_by_default:         1,
+          ssh_server_domain:                     default_hostname,
+          http_server_domain:                    default_hostname,
+          https_server_domain:                   default_hostname,
+          http_server_subdir:                    '',
+          show_repositories_url:                 true,
+          gitolite_daemon_by_default:            false,
+          gitolite_http_by_default:              1,
           
           # Redmine Config
-          redmine_has_rw_access_on_all_repos: true,
-          all_projects_use_git:               false,
-          init_repositories_on_create:        false,
-          delete_git_repositories:            true,
+          redmine_has_rw_access_on_all_repos:    true,
+          all_projects_use_git:                  false,
+          init_repositories_on_create:           false,
+          delete_git_repositories:               true,
           
           # This params work together!
           # When hierarchical_organisation = true unique_repo_identifier MUST be false
           # When hierarchical_organisation = false unique_repo_identifier MUST be true
-          hierarchical_organisation:        true,
-          unique_repo_identifier:           false,
+          hierarchical_organisation:             true,
+          unique_repo_identifier:                false,
           
           # Download Revision Config
-          download_revision_enabled:        true,
+          download_revision_enabled:             true,
           
           # Git Mailing List Config
           gitolite_notify_by_default:            false,
@@ -99,7 +99,7 @@ module OpenProject::Revisions::Git
           gitolite_use_sidekiq:                  false,
 
           # Delayed jobs
-          use_delayed_jobs: false,
+          use_delayed_jobs:                      false,
         }
       }
     end
